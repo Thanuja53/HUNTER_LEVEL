8eff430eb5448e6512af539a713d104f967a1c4a
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-11 06:10:26 +0530

    Update unique_no.java

--- unique_no.java
+++ unique_no.java
@@ -4,30 +4,32 @@ class unique_no
   public static void main(String args[])
   {
     Scanner in=new Scanner(System.in);
-    System.out.println("ENTER THE SIZE OF ARRAY")
+    System.out.println("ENTER THE SIZE OF ARRAY");
     int n=in.nextInt();
     int[] no=new int[n];
-    System.out.println("ENTER THE NUMBERS FOR ARRAY")
+    System.out.println("ENTER THE NUMBERS FOR ARRAY");
     for(int i=0;i<n;i++)
     {
       no[i]=in.nextInt();
     }
     for(int j=0;j<n;j++)
     {
+      int a=0,b=0;
       int flag=0;
-      int a=no[j];
+      a=no[j];
           for(int k=0;k<n;k++)
           {
-            if(no[j]==no[k])
+			  b=no[k]; 
+            if(a==b)
             {
               flag++;
             }
           }
-      if(flag==0)
-         System.out.println("THE UNIQUE NUMBER IS"+no[j])
+      if(flag==1)
+	  {
+         System.out.println("THE UNIQUE NUMBER IS "+a);
+		 break;
+	  }
     }
   }
 }
-    
-        
-      
