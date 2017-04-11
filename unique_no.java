aa02424746c9dcbe5681eb7c0d09ef47d1720d71
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-11 06:02:16 +0530

    Update unique_no.java

--- unique_no.java
+++ unique_no.java
@@ -1,1 +1,33 @@
-# HUNTER_LEVEL
+import java.util.*;
+class unique_no
+{
+  public static void main(String args[])
+  {
+    Scanner in=new Scanner(System.in);
+    System.out.println("ENTER THE SIZE OF ARRAY")
+    int n=in.nextInt();
+    int[] no=new int[n];
+    System.out.println("ENTER THE NUMBERS FOR ARRAY")
+    for(int i=0;i<n;i++)
+    {
+      no[i]=in.nextInt();
+    }
+    for(int j=0;j<n;j++)
+    {
+      int flag=0;
+      int a=no[j];
+          for(int k=0;k<n;k++)
+          {
+            if(no[j]==no[k])
+            {
+              flag++;
+            }
+          }
+      if(flag==0)
+         System.out.println("THE UNIQUE NUMBER IS"+no[j])
+    }
+  }
+}
+    
+        
+      
