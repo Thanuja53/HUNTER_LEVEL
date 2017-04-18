2f992b862d7a7582404c52cc31d6ef2bec36cbc2
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-04-18 22:45:33 +0530

    Update oddreverse72.java

--- oddreverse72.java
+++ oddreverse72.java
@@ -1,1 +1,25 @@
-
+import java.util.*;
+class oddreverse72
+{
+  public static void main(String args[])
+  {
+	int a=1;
+	Scanner in=new Scanner(System.in);
+	System.out.println("ENTER A STRING");
+	String s=in.nextLine();
+	String[] str=s.split(" ");
+    for(String s1:str)
+	{
+		if(a%2!=0)
+		{
+	    StringBuffer sb=new StringBuffer(s1);
+		System.out.print(sb.reverse()+" ");
+		}
+		else
+		{
+		System.out.print(s1+" ");
+		}
+		a++;
+	}
+  }
+}
