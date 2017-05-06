b8d5f021fac73747d9f6c41fe6fd27fbecde4699
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-05-06 07:15:19 +0530

    Update reverse11.java

--- reverse11.java
+++ reverse11.java
@@ -1,1 +1,11 @@
-
+class reverse11
+{
+  public static void main(String args[])
+  {
+    String[] s=args[0].split(" ");
+    for(int i=args[0].length;i>=0;i--)
+    {
+      System.out.println(s[i]);
+    }
+  }
+    
