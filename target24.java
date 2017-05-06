e49c3bdb3176e7056ce6e25600b6071506303b03
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-05-06 07:39:30 +0530

    Update target24.java

--- target24.java
+++ target24.java
@@ -1,1 +1,23 @@
-
+import java.util.*;
+class target24 
+{
+  public static void main(String args[])
+  {
+    Scanner get=new Scanner(System.in);
+    System.out.print("enter the target number");
+    int no=get.nextInt();
+    for(int i=0;i<args.length;i++)
+    {
+      for(int j=0;j<args.length;j++)
+      {
+        if(i!=j)
+        {
+          if(args[i]+args[j]==no)
+          {
+            System.out.println(+args[i]+"and"+args[j]);
+          }
+        }
+      }
+    }
+  }
+}
