92c9124f13ef33c545dd028a4f7a63a25e0ad94b
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-02 07:20:14 +0530

    Update hunter69.java

--- hunter69.java
+++ hunter69.java
@@ -1,1 +1,36 @@
+import java.util.Scanner;
+
+public class hunter69 {
+
+	public static void main(String[] args) {
+		System.out.println("ENTER THE EMAIL ID");;
+		Scanner in=new Scanner(System.in);
+		String s=in.nextLine();
+		int count=0;
+		if(s.endsWith(".com"))
+		{
+			for(int i=0;i<s.length();i++)
+			{
+				if(s.charAt(i)=='@')
+				{
+					count++;
+				}
+			}	
+		if(count==1)
+		{
+			int x=s.lastIndexOf('@');
+			int y=s.lastIndexOf('.');
+			if(y-x==6)
+			{
+				String s1=s.substring(0, x);
+				int l1=s1.length();
+				if(l1>=3)
+				{
+					System.out.println("EMAIL IS VALID");
+				}	
+			}
+		}
+		}
+	}
+}
 
