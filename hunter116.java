0194b4c29b6c5d8a8f6138de30e8961d948a6182
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-02 14:55:15 +0530

    Update hunter116.java

--- hunter116.java
+++ hunter116.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
+
+public class hunter116 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("ENTER THE NUMBER");
+		int n=in.nextInt();
+		int sum=0;
+		String s=String.valueOf(n);
+		int x=0,y=0;
+		for(String s1:s.split(""))
+		{
+			if(x==0)
+				{
+				System.out.print(s1+"^"+(x++));
+				}
+			
+			else
+			{
+			    System.out.print("+"+s1+"^"+(x++));
+			}
+			sum=(Integer.valueOf(s1)*y++)+sum;
+			
+		}
+		
+		System.out.print("=="+sum);
+	}
+
+}
 
