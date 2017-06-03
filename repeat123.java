51bbd98cdce45cf750fd10a1356ae14ef5aa0e34
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-03 10:05:37 +0530

    Update repeat123.java

--- repeat123.java
+++ repeat123.java
@@ -1,1 +1,32 @@
+import java.util.*;
+class repeat123
+{
+    public static void main(String args[])
+    {
+        {
+        Scanner in=new Scanner(System.in);
+    	System.out.println("ENTER THE STRING");
+   		String s=in.nextLine();
+        char[] c=s.toCharArray();
 
+        for(int i=0;i<s.length();i++)
+        {
+            int flag=0;
+        	char ch=s.charAt(i);
+        	for(int j=0;j<s.length();j++)
+        	{
+        		if(ch==c[j])
+        		{
+        			flag++;
+        		}
+        	}
+        	if(flag==1)
+        	{
+        		System.out.print(c[i]);
+        	}
+        }
+       
+        
+        }
+    }
+}
