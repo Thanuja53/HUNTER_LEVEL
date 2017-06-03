6030c8c984b7d42e2c6f6f60f2f785cf2f0d96a3
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-03 10:32:37 +0530

    Update day130.java

--- day130.java
+++ day130.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class day130 {
+
+	public static void main(String[] args) {
+		String[] s={"sun","mon","tue","thu","fri","sat"};
+		Scanner in=new Scanner(System.in);
+		System.out.println("enter the day");
+		String s1=in.nextLine();
+			if(s[0].toUpperCase().equals(s1.toUpperCase()))
+			{
+				System.out.println(s1+"=FALSE");
+			}
+			else
+		{
+			System.out.println(s1+"=TRUE");
+		}
+
+	}
+
+}
 
