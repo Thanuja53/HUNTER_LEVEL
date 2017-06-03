e2ef389d99d960d42ce5df70d7a02440f664d2ef
Rathina Moorthy Nataraj <nrathinamoorthy@gmail.com>
2017-06-03 23:43:59 +0530

    Update multiply51.java

--- multiply51.java
+++ multiply51.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class multiply51 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+    	System.out.println("ENTER THE STRING 1");
+   		String s=in.nextLine();
+   		System.out.println("ENTER THE STRING 2");
+   		String s1=in.nextLine();
+   		System.out.print(String.valueOf(Integer.valueOf(s)*Integer.valueOf(s1)));
+   		
+
+	}
+
+}
 
